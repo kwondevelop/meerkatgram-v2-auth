3bf9149bc5e9e06dc8402c6fd00074165c0c541e
coding_babo <kwondevelop@gmail.com>
2026-08-05 12:41:18 +0900

    del 불필요 파일

--- src/main/java/com/msa4meerkatgramv2auth/global/config/SubServiceURIConfig.java
+++
@@ -1,8 +0,0 @@
-package com.msa4meerkatgramv2auth.global.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "sub-service")
-public record SubServiceURIConfig(
-    String frontendCallbackUri
-) {}
